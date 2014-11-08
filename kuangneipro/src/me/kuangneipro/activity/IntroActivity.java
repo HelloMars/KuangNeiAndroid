fc708fcfd9fc66196a5579276d6748c7d64aa122
lj8215946 <luji90@126.com>
2014-11-09 01:57:25 +0800

    ok，不改了，睡吧

--- kuangneipro/src/me/kuangneipro/activity/IntroActivity.java
+++ kuangneipro/src/me/kuangneipro/activity/IntroActivity.java
@@ -1,14 +1,18 @@
 package me.kuangneipro.activity;
 
-import com.igexin.sdk.PushManager;
-
 import me.kuangneipro.R;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
 import android.view.View.OnClickListener;
 
+import com.igexin.sdk.PushManager;
+
+@SuppressLint("HandlerLeak") 
 public class IntroActivity extends Activity implements OnClickListener{
 
 	private View intro;
@@ -20,8 +24,22 @@ public class IntroActivity extends Activity implements OnClickListener{
 		//个推请求clientid,并注册接收监听
         PushManager.getInstance().initialize(this.getApplicationContext());
 		intro = findViewById(R.id.intro);
+		intro.setEnabled(false);
 		intro.setOnClickListener(this);
+		
+		 
+		handler.sendEmptyMessageDelayed(0, 2000);
 	}
+	
+	private Handler handler = new Handler() {
+		 
+        @Override
+        public void handleMessage(Message msg) {
+        	if(intro!=null)
+        		intro.setEnabled(true);
+            super.handleMessage(msg);
+        }
+    };
 
 	@Override
 	public void onClick(View arg0) {
